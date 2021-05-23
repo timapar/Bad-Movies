2fd5665eb27a55bccde54c19aa546ad4d4c02130
timapar <timothy_parrish@hotmail.com>
2021-05-23 12:36:17 -0400

    choose option 2

--- server/index.js
+++ server/index.js
@@ -29,38 +29,6 @@ Use the routes below to build your application:
 
 */
 
-//TODO: Pick one of the two route options below:
-//OPTION 1: Use regular routes, where endpoints are pre-defined on this page, you do NOT need to refer to /server/routes/movieRoutes.js file
-//OPTION 2: Use Express Router, where the routes are defined under /server/routes/movieRoutes.js file
-
-//***********************************************************************************************************************
-//OPTION 1: Use regular routes;
-//If you are using OPTION 1, you do not need routes>movieRoutes.js file
-
-app.get("/genres", function(req, res) {
-  // make an axios request to get the official list of genres from themoviedb
-  // use this endpoint. you will need your API key from signup: https://api.themoviedb.org/3/genre/movie/list
-});
-
-app.get("/search", function(req, res) {
-  // use this endpoint to search for movies by genres (using API key): https://api.themoviedb.org/3/discover/movie
-  // and sort them by votes (worst first) using the search parameters in themoviedb API
-  // do NOT save the results into the database; render results directly on the page
-});
-
-app.post("/save", function(req, res) {
-  //save movie as favorite into the database
-});
-
-app.post("/delete", function(req, res) {
-  //remove movie from favorites into the database
-});
-
-//***********************************************************************************************************************
-//OPTION 2: Use Express Router
-
-//IF you decide to go with this OPTION 2, delete OPTION 1 to continue
-
 //Routes
 const movieRoutes = require("./routes/movieRoutes.js");
 
