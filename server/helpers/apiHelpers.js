e52d2b8153406443bea764d591bc8ac8cef71d4e
timapar <timothy_parrish@hotmail.com>
2021-05-23 12:35:18 -0400

    implement getGenreList and getMovieListByGenre

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -2,6 +2,9 @@ const request = require('request');
 const axios = require('axios');
 const { API_KEY } = require('../../config.js');
 
+const API = axios.create({
+  baseURL: `https://api.themoviedb.org/3`
+});
 // write out logic/functions required to query TheMovieDB.org
 
 // FOR REFERENCE:
@@ -10,3 +13,10 @@ const { API_KEY } = require('../../config.js');
 // Get your API Key and save it in your config file
 
 // Don't forget to export your functions and require them within your server file
+
+module.exports.getGenreList = () => {
+  api.get(`/genre/movie/list?api_key=${API_KEY}&language=en-US`);
+}
+module.exports.getMovieListByGenre = (genre) => {
+  api.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.asc&with_genres=${genre}`);
+}
