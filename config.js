c6a89642987b37f875ff0a75cdd8c711f36e4d5a
timapar <timothy_parrish@hotmail.com>
2021-05-23 12:39:41 -0400

    remove config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'P@ssw0rd',
   database: 'badmovies',
-  API_KEY: '',
+  API_KEY: '7f2a988073f0a8112d0ab398b0da93e1',
 };
